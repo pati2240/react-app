d8f657487726898f85268ae8bb8ce472ed32ab65
pati2240 <87320963+pati2240@users.noreply.github.com>
2023-11-12 20:37:58 +0330

    react-app

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Alert from "./components/Alert";
+function App() {
+  return (
+    <div>
+      <Alert>
+        <span>hiw</span>eb
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/Message.tsx
@@ -0,0 +1,4 @@
+function Message(){
+    return <h1 className="mt-5">Hello World</h1>
+}
+export default Message;
