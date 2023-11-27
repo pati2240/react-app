ba9e67fa1696ab919cefbd43276eb072f56319bd
pati2240 <87320963+pati2240@users.noreply.github.com>
2023-11-27 12:58:52 +0330

    n

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,37 +1,10 @@
 import { useState } from "react";
+import Title from "./components/Title";
 
 function App() {
- 
-  const [showEvents, setShowEvents] = useState(true);
 
-  const [events, setEvents] = useState([
-    { title: "Spiderman 2023", id: 1 },
-    { title: "need for speed 2006", id: 2 },
-    { title: "last of us 2 2020", id: 3 },
-    { title: "batman 2022", id: 4 },
-  ]);
-  const handlClick = (id) => {
-    setEvents((prevEvents) => {
-      return prevEvents.filter((event) => {
-        return id !== event.id
-      })
-    })
-  };
   return (
-    <>
-      <div className="App">
-        <div><button onClick={()=> setShowEvents(false)}>Hide</button></div>
-        <div><button onClick={()=> setShowEvents(true)}>Show</button></div>
-        {showEvents && events.map((event, index) => (
-          <div key={event.id}>
-            <h2>
-              {index + 1} - {event.title}
-            </h2>
-            <button onClick={() => handlClick(event.id)}>Delete</button>
-          </div>
-        ))}
-      </div>
-    </>
+   <div></div>
   );
 }
 
---
+++ vite-project/src/components/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title() {
+    return (
+        <div>
+            <h1 className="title">best games</h1>
+            <p className="subtitle">latest games</p>
+        </div>
+    )
+}
